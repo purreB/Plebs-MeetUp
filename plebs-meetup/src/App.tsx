771c2b8b27d99dac2ad555e785ce6b42ad3e3742
MugiwaraDre <atd.hirsch@gmail.com>
2022-01-21 10:22:22 +0100

    [Dre] | added popup test

--- plebs-meetup/src/App.tsx
+++ plebs-meetup/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import BaseModalWrapper from './components/Modal/BaseModalWrapper';
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const toggleModal = () => {
+    setIsModalVisible((wasModalVisible) => !wasModalVisible);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={toggleModal}>Show modal test</button>
+      <BaseModalWrapper
+        isModalVisible={isModalVisible}
+        onBackdropClick={toggleModal}
+      />
     </div>
   );
 }
