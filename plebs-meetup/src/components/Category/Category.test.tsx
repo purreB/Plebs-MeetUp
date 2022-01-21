30c7f22783664d744bc389f57a4686b084143540
purreB <jakob.bergstrom@live.se>
2022-01-21 15:25:03 +0100

    Category testing done, might add more later

--- plebs-meetup/src/components/Category/Category.test.tsx
+++ plebs-meetup/src/components/Category/Category.test.tsx
@@ -1,8 +1,32 @@
 import Categories from './Categories';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('Category list component', () => {
+  const setup = () => render(<Categories />);
+
   it('renders without crashing', () => {
-    render(<Categories />);
+    setup();
+  });
+
+  it('renders correct amount of categories', () => {
+    setup();
+    const items = screen.getAllByRole('listitem');
+    expect(items.length).toBe(5);
+  });
+
+  it('A first time user gets saved to LocalStorage', () => {
+    setup();
+    const fetchedUser = JSON.parse(localStorage.getItem('User')!);
+    expect(fetchedUser).toHaveProperty('id' && 'name');
+  });
+
+  // when a category is clicked, set favorite to category in LocalStorage
+  it('when a category is clicked, it gets added to user favorite', () => {
+    setup();
+    const chosenCategory = screen.getByText('MUSIC');
+    userEvent.click(chosenCategory);
+    const fetchedUser = JSON.parse(localStorage.getItem('User')!);
+    expect(fetchedUser).toHaveProperty('id' && 'name' && 'favorite', 'Music');
   });
 });
